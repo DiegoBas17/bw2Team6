fa475e07dd2619e271bff005663800f340b52cf2
Gius3pp33 <peppe-2@live.it>
2024-06-21 12:19:42 +0200

    cambio il bottone in base a play o pausa

--- homePage.js
+++ homePage.js
@@ -346,14 +346,18 @@ function loadTrack(index) {
 }
 
 function playPauseTrack() {
+ let icon = document.getElementById('play-icon');
+  
   if (isPlaying) {
-    audio.pause();
-    isPlaying = false;
-    document.getElementById("play-pause").src = "play-icon.png";
+      audio.pause();
+      isPlaying = false;
+      // Cambia l'icona a play
+      icon.setAttribute('d', 'M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814z');
   } else {
-    audio.play();
-    isPlaying = true;
-    document.getElementById("play-pause").src = "pause-icon.png";
+      audio.play();
+      isPlaying = true;
+      // Cambia l'icona a pausa
+      icon.setAttribute('d', 'M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5m3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5"');
   }
 }
 
